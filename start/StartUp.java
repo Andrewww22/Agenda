d4e8a20319929da4e81d834b97ea531393d177be
Andrewww22 <32323132+Andrewww22@users.noreply.github.com>
2017-11-02 19:20:46 +0200

    Package start
    This package contains the java file that will start the application;

---
+++ start/StartUp.java
@@ -0,0 +1,27 @@
+package start;
+
+import javax.swing.JFrame;
+
+public class StartUp extends  JFrame{
+
+	int startX=300;
+	int startY = 300;
+	int dim = 300;
+	
+	public StartUp(int startX, int startY , int dim) 
+	{
+		this.setBounds(startX, startY, startX+dim, startY+dim);
+		this.setTitle("Agenda");
+		this.setVisible(true);
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StartUp st = new  StartUp(100, 200, 300);
+
+	}
+
+}
