20cbf0ccac8efa78e36fe8126b769089964c85a7
Andrewww22 <32323132+Andrewww22@users.noreply.github.com>
2017-11-03 15:03:50 +0200

    Create view package

---
+++ view/Contact.java
@@ -0,0 +1,20 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class Contact extends ViewTools{
+	
+	
+
+	public Contact(int dimX, int dimY)
+	{
+		super(dimX,dimY);
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Contact ct = new Contact(200, 300);
+	} 
+
+}
