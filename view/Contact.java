bab5182be118b5bfd914d34661f3d6ed0c674f4d
Andrewww <andreitranca@gmail.com>
2017-11-07 12:38:40 +0200

    Adding label  and textfield to view.Contact
    Addding a cotainer .
    To that container a label and a textfield is added .

--- view/Contact.java
+++ view/Contact.java
@@ -1,20 +1,34 @@
 package view;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Contact extends ViewTools{
-	
-	
+
+
 
 	public Contact(int dimX, int dimY)
 	{
 		super(dimX,dimY);
+		this.setTitle("Contact" +"-Operation");
+
+		Container container = getContentPane();
+
+		JLabel labelText = new JLabel();
+		labelText.setText("First Name: ");
+		container.add(labelText);
+
+		JTextField field = new JTextField(10);
+		container.add(field,BorderLayout.SOUTH);
 	}
-	
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       Contact ct = new Contact(200, 300);
-	} 
+	}
 
 }
