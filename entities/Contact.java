8813e2259b24d5ebd9fe11b7b2c11a8d6f217042
Andrewww <andreitranca@gmail.com>
2017-11-07 11:36:04 +0200

    Adding the Contact java Class
    Adding the entities package;
    Creating the entity Contact;
    Addin the fields: FirstName, LastName etc.

---
+++ entities/Contact.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class Contact {
+
+	/*Nume
+	//String lastName;
+	//*Prenume
+	//String firstName;
+	//Numar
+   // String phoneNumber;
+	//*Adresa mail
+	//String mail;
+	 */
+
+	String FirstName;
+	String LastName;
+	String MobilePhoneNumber;
+
+
+
+	public Contact()
+	{}
+}
