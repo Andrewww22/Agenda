3b80fe1bcc18846cd73cff18ee7d4132619c5a1b
Andrewww <andreitranca@gmail.com>
2017-11-07 11:07:44 +0200

    Updating StartUp with menuitem
    StartUp extends ViewToolos now, It doesn't need the starting positions.
    
    A menu item (New contact) is added and and action lstener is added to that item

--- start/StartUp.java
+++ start/StartUp.java
@@ -1,47 +1,51 @@
 //The starting file of the application
 package start;
 
-import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-public class StartUp extends  JFrame{
+public class StartUp extends  ViewTools
+{
 
-	int startX=300;
-	int startY = 300;
-	int dim = 300;
-	
-	public StartUp(int startX, int startY , int dim) 
+	public StartUp(int dimX, int dimY)
 	{
-		this.setBounds(startX, startY, startX+dim, startY+dim);
+		super(dimX, dimY);
 		this.setTitle("Agenda");
+
 		JMenuBar menuBar;
 		JMenu menu ;
 		JMenuItem menuItem;
-		
+
 		//Create the menu bar;
 		menuBar = new  JMenuBar();
-		
+
 		//Build the first menu;
 		menu = new JMenu("Contact");
-		
+
 		//addding the menu to the menu bar
 		menuBar.add(menu);
-		
+
+		//adding an item to the new menu
+		menuItemNew = new JMenuItem(MenuTools.MENU_CONTACT_NEW);
+		//Adds shortcut
+		menuItemNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.ALT_MASK));
+		menuItemNew.addActionListener(new MenuActions());
+		menu.add(menuItemNew);
+
 		setJMenuBar(menuBar);
-		
-		
-		
+
+
+
 		this.setVisible(true);
-		
+
 	}
-	
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		StartUp st = new  StartUp(100, 200, 300);
+
+		StartUp st = new  StartUp(300,200);
 
 	}
 
