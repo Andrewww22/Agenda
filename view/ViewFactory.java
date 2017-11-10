f01e070b5dd54c877130657f7d8bf7c7fe590d13
Andrewww <andreitranca@gmail.com>
2017-11-10 13:44:12 +0200

    Twiking the layout of the ViewFactory
    Adding a subpanel for each grup of label and text field.
    Using the BoxLayout .
    Adding between the label and text field a horizontal invisible struct.

--- view/ViewFactory.java
+++ view/ViewFactory.java
@@ -18,24 +18,41 @@ public class ViewFactory
     GeneralView gv = new GeneralView(100,100);
     if (who.equals("Contact") )
 		{
-			      gv.setTitle( who +"-Operation");
-            Contact  contact = new Contact();
+			   Contact  contact = new Contact();
+			   gv.setTitle( who +"-Operation");
 
-				    Container container = gv.getContentPane();
-            int  dim =  contact.attributeKey.length;
-				    for(int i = 0 ; i < dim ; i++)
-				    {
-				          JLabel aTextlabel = new JLabel();
-				          aTextlabel.setText("First Name: ");
-				          container.add(aTextlabel);
-                  if(contact.attributeType[i].equals("text"))
-						      {
-                      JTextField  aTextField = new JTextField(10);
-                      container.add(aTextField, BorderLayout.SOUTH);
-                  }
-		        }
 
-            return gv;
+				//Container container = gv.getContentPane();
+			   	JPanel container= new JPanel();
+				gv.setContentPane(container);
+				gv.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+
+
+
+
+				int  dim =  contact.attributeKey.length;
+				for(int i = 0 ; i < dim ; i++)
+				{
+						JPanel aPanel = new JPanel();
+
+						JLabel aTextLabel = new JLabel();
+						aTextLabel.setText(contact.attributeLabel[i]);
+						aPanel.add(aTextLabel);
+
+
+						//container.add(aTextLabel);
+
+						if(contact.attributeType[i].equals("text"))
+						{
+							JTextField  aTextField = new JTextField(10);
+							aPanel.add(Box.createHorizontalStrut(40));
+							aPanel.add(aTextField);
+							//container.add(aTextField, BorderLayout.SOUTH);
+						}
+
+						container.add(aPanel);
+						gv.pack();
+				}
     }
 	public static void main(String[] args)
 	{
